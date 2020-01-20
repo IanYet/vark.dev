6c8497ee970205cbc7a07ab0b855f91911b45872
IanYet <yt2015.97@gmail.com>
2020-01-20 20:45:23 +0800

    inti

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import imgUrl from './assets/bg.jpg'
+
+function App() {
+    return (
+        <div>
+            react A
+            <img src={imgUrl} />
+        </div>
+    )
+}
+
+export default App
