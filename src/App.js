481a6c53b0023371eecb4937f2a6494d36e63341
IanYet <yt2015.97@gmail.com>
2020-01-21 08:03:21 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import style from  './App.module.css'
 import './App.css'
 import imgUrl from './assets/bg.jpg'
 
 function App() {
-    console.log(style)
     return (
-        <div className={style.app}>
+        <div className='app'>
             react A
             {/* <img src={imgUrl} /> */}
         </div>
