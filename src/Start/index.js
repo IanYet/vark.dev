d054fd2ed8343457f1ce1856c4f6fd5da5432fc5
IanYet <yt2015.97@gmail.com>
2020-01-28 13:48:22 +0800

    update

--- src/Start/index.js
+++ src/Start/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 function Start() {
     return (
-        <div>
+        <div className='start-container'>
             <div className='title-container'>
                 <div className='title one'>你好</div>
                 <div className='title two'>我是一日渚</div>
